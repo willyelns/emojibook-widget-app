b5adcdb31edf29a3d00872511d4b34b5575f1707
Willyelns Xavier <willyelnsg.x@gmail.com>
2020-09-03 11:00:56 -0300

    Initial Commit

---
+++ Emojibook/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Emojibook
+//
+//  Created by Will Xavier on 03/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Emojibook/EmojibookApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojibookApp.swift
+//  Emojibook
+//
+//  Created by Will Xavier on 03/09/20.
+//
+
+import SwiftUI
+
+@main
+struct EmojibookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
